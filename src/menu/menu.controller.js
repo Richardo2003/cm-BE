e17508cf519fe9c15cd91b947729e595fff5463d
tinotoon <s22110469@student.unklab.ac.id>
2024-05-08 13:25:08 +0800

    menambahkan end point pada tabel menu

--- src/menu/menu.controller.js
+++ src/menu/menu.controller.js
@@ -1,1 +1,45 @@
-// fallen
+const express = require("express");
+const prisma = require("../db/index.js");
+const router = express.Router();
+
+// GET all menus
+router.get("/", async (req, res) => {
+  const menu = await prisma.Menu.findMany();
+  res.json(menu);
+});
+
+// GET a specific menu by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const menu = await prisma.Menu.findUnique({ where: { id: parseInt(id) } });
+  res.json(menu);
+});
+
+// CREATE a new menu
+router.post("/", async (req, res) => {
+  const { nama, harga, deskripsi } = req.body;
+  const newMenu = await prisma.Menu.create({
+    data: { nama, harga, deskripsi },
+  });
+  res.json(newMenu);
+});
+
+// UPDATE a menu by id
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nama, harga, deskripsi } = req.body;
+  const updatedMenu = await prisma.Menu.update({
+    where: { id: parseInt(id) },
+    data: { nama, harga, deskripsi },
+  });
+  res.json(updatedMenu);
+});
+
+// DELETE a menu by id
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedMenu = await prisma.Menu.delete({ where: { id: parseInt(id) } });
+  res.json(deletedMenu);
+});
+
+module.exports = router;
--- src/menu/menu.repository.js
+++ src/menu/menu.repository.js
@@ -1,1 +1,34 @@
-// fallen
+const prisma = require("../db/index.js");
+
+const findMenus = async () => {
+  const menus = await prisma.Menu.findMany();
+  return menus;
+};
+
+const findMenuById = async (id) => {
+  const menu = await prisma.Menu.findUnique({ where: { id } });
+  return menu;
+};
+
+const createMenu = async (data) => {
+  const newMenu = await prisma.Menu.create({ data });
+  return newMenu;
+};
+
+const updateMenu = async (id, data) => {
+  const updatedMenu = await prisma.Menu.update({ where: { id }, data });
+  return updatedMenu;
+};
+
+const deleteMenu = async (id) => {
+  const deletedMenu = await prisma.Menu.delete({ where: { id } });
+  return deletedMenu;
+};
+
+module.exports = {
+  findMenus,
+  findMenuById,
+  createMenu,
+  updateMenu,
+  deleteMenu,
+};
