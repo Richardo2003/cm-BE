1766208289eb705e6e385a0821196b09be6faf1b
Rionaldo16 <siamanrionaldo16@gmail.com>
2024-05-08 13:45:43 +0800

    Menambahkan get semua dan post

--- src/itempesanan/itemPesanan.controller.js
+++ src/itempesanan/itemPesanan.controller.js
@@ -1,1 +1,31 @@
-// rio & juan
+const {
+    getAllItemsPesanan,
+    createNewItemPesanan,
+    getItemPesananById
+  
+  } = require("./itemPesanan.service");
+  const router = require("express").Router();
+  
+  router.get("/", async (req, res) => {
+    const itemsPesanan = await getAllItemsPesanan();
+    res.json(itemsPesanan);
+  });
+  
+  router.get("/:id", async (req, res) => {
+    const itemPesanan = await getItemPesananById(req.params.id);
+    res.json(itemPesanan);
+  });
+  
+  router.post("/", async (req, res) => {
+    try {
+      const newItemPesanan = await createNewItemPesanan(req.body);
+      res.json(newItemPesanan);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+ 
+  
+  module.exports = router;
+  
--- src/itempesanan/itemPesanan.repository.js
+++ src/itempesanan/itemPesanan.repository.js
@@ -1,1 +1,34 @@
-// rio & juan
+const prisma = require("../db/index.js");
+
+const findItemsPesanan = async () => {
+  const itemsPesanan = await prisma.ItemPesanan.findMany();
+  return itemsPesanan;
+};
+
+const findItemPesananById = async (id) => {
+  const itemPesanan = await prisma.ItemPesanan.findUnique({
+    where: { id: parseInt(id) },
+  });
+  return itemPesanan;
+};
+
+const createItemPesanan = async (data) => {
+  const existingPesanan = await prisma.Pesanan.findUnique({
+    where: { id: data.pesananId },
+  });
+
+  if (!existingPesanan) {
+    throw new Error("Pesanan dengan ID tersebut tidak ditemukan");
+  }
+
+  const newItemPesanan = await prisma.ItemPesanan.create({ data });
+  return newItemPesanan;
+};
+
+
+
+module.exports = {
+  findItemsPesanan,
+  findItemPesananById,
+  createItemPesanan,
+};
